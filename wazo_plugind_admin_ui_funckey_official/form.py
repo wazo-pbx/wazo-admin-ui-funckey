52a235199f83dd239c74e66a53ebf76897ff89b3
Cedric Abunar <cabunar@wazo.io>
2018-01-10 14:26:15 -0500

    add custom funckey destinations

--- wazo_plugind_admin_ui_funckey_official/form.py
+++ wazo_plugind_admin_ui_funckey_official/form.py
@@ -1,15 +1,11 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-from flask_babel import lazy_gettext as l_
-from wtforms.fields import (SubmitField,
-                            StringField)
+from wtforms.fields import StringField
 from wtforms.validators import InputRequired, Length
 
 from wazo_admin_ui.helpers.form import BaseForm
 
 
-class FuncKeyTemplateForm(BaseForm):
-    name = StringField(l_('Name'), [InputRequired(), Length(max=128)])
-    keys = StringField()
-    submit = SubmitField()
+class CustomFuncKeyDestination(BaseForm):
+    exten = StringField(validators=[InputRequired(), Length(max=255)])
--- wazo_plugind_admin_ui_funckey_official/plugin.py
+++ wazo_plugind_admin_ui_funckey_official/plugin.py
@@ -1,12 +1,15 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+from flask_babel import lazy_gettext as l_
 from flask_menu.classy import register_flaskview
 
+from wazo_admin_ui.helpers.funckey import register_funckey_destination_form
 from wazo_admin_ui.helpers.plugin import create_blueprint
 
 from .service import FuncKeyTemplateService
 from .view import FuncKeyTemplateView
+from .form import CustomFuncKeyDestination
 
 funckey = create_blueprint('funckey', __name__)
 
@@ -19,5 +22,7 @@ class Plugin(object):
         FuncKeyTemplateView.service = FuncKeyTemplateService()
         FuncKeyTemplateView.register(funckey, route_base='/funckeys')
         register_flaskview(funckey, FuncKeyTemplateView)
+
+        register_funckey_destination_form('custom', l_('Custom'), CustomFuncKeyDestination)
 
         core.register_blueprint(funckey)
