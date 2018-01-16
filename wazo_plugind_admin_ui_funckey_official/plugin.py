557c37cff71a37f97a98d00d458444cd5d88bc3b
Cedric Abunar <cabunar@wazo.io>
2018-01-16 14:11:12 -0500

    add services destination funckeys

--- wazo_plugind_admin_ui_funckey_official/plugin.py
+++ wazo_plugind_admin_ui_funckey_official/plugin.py
@@ -1,4 +1,4 @@
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from flask_babel import lazy_gettext as l_
@@ -9,7 +9,12 @@ from wazo_admin_ui.helpers.plugin import create_blueprint
 
 from .service import FuncKeyTemplateService
 from .view import FuncKeyTemplateView
-from .form import CustomFuncKeyDestination
+from .form import CustomFuncKeyDestination, \
+    GeneralServicesFuncKeyDestinationForm, \
+    TransferServicesFuncKeyDestinationForm, \
+    OnlineRecFuncKeyDestinationForm, \
+    ForwardServicesFuncKeyDestinationForm
+
 
 funckey = create_blueprint('funckey', __name__)
 
@@ -24,5 +29,9 @@ class Plugin(object):
         register_flaskview(funckey, FuncKeyTemplateView)
 
         register_funckey_destination_form('custom', l_('Custom'), CustomFuncKeyDestination)
+        register_funckey_destination_form('transfer', l_('Transfer'), TransferServicesFuncKeyDestinationForm)
+        register_funckey_destination_form('service', l_('Service'), GeneralServicesFuncKeyDestinationForm)
+        register_funckey_destination_form('forward', l_('Forward'), ForwardServicesFuncKeyDestinationForm)
+        register_funckey_destination_form('onlinerec', l_('Online Recording'), OnlineRecFuncKeyDestinationForm)
 
         core.register_blueprint(funckey)
